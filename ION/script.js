9a6e1d71b17976a3b55aa570f28e0b99ad67c5ca
최지웅 <jiwoongchoi@choejiuimacmini.local.ftr>
2025-12-12 09:50:07 -0600

    Fix: Remove DOMContentLoaded listener for Shopify

--- ION/script.js
+++ ION/script.js
@@ -1,24 +1,23 @@
-/* [최종 수정] 깃허브 절대 주소를 사용하여 쇼피파이에서도 이미지를 찾게 함 */
+/* [최종 수정] 쇼피파이에서 즉시 실행되도록 수정함 */
 
-// 1. 깃허브 주소 변수 (이게 있어야 쇼피파이가 사진 위치를 알아요!)
-// (맨 뒤에 슬래시 / 가 있는지 꼭 확인하세요)
+// 1. 깃허브 주소 변수
 var baseURL = "https://RobinChoi00.github.io/Product/ION/";
 
 var slideData = [
     {
-        // 0번: All (파일명/확장자 확인: png)
+        // 0번: All
         chair: baseURL + "images/0.ion_color_both.png", 
         btnNormal: baseURL + "images/7.ion_swatch_all.png",
         btnSelected: baseURL + "images/10.ion_swatch_all_selected.png"
     },
     {
-        // 1번: Black (파일명/확장자 확인: png)
+        // 1번: Black
         chair: baseURL + "images/3.ion_color_black.png", 
         btnNormal: baseURL + "images/8.ion_swatch_black.png",
         btnSelected: baseURL + "images/11.ion_swatch_black_selected.png"
     },
     {
-        // 2번: Brown (파일명/확장자 확인: png)
+        // 2번: Brown
         chair: baseURL + "images/2.ion_color_brown.png", 
         btnNormal: baseURL + "images/9.ion_swatch_brown.png",
         btnSelected: baseURL + "images/12.ion_swatch_brown_selected.png"
@@ -36,7 +35,6 @@ function updateDisplay() {
     if(imgEl) {
         imgEl.style.opacity = 0.5;
         setTimeout(function() {
-            // baseURL이 포함된 전체 주소가 들어갑니다
             imgEl.src = slideData[currentIndex].chair; 
             imgEl.style.opacity = 1;
         }, 150);
@@ -56,7 +54,7 @@ function updateDisplay() {
     }
 }
 
-// 3. 전역 함수 등록
+// 3. 전역 함수 등록 (window.함수이름)
 window.moveSlide = function(direction) {
     currentIndex += direction;
     if (currentIndex >= slideData.length) currentIndex = 0;
@@ -69,6 +67,5 @@ window.changeColor = function(index) {
     updateDisplay();
 };
 
-// 4. 초기화 실행
-// (페이지가 뜨자마자 버튼 상태를 맞추기 위해 실행)
+// 4. 로드 되자마자 즉시 실행 (대기 없이 바로 시작)
 updateDisplay();
